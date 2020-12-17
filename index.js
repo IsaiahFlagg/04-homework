794e1d49ef84de3b1bcf461ed33f0ad75cb61cb0
Isaiah Flagg <IsaiahNFlagg@gmail.com>
2020-12-16 20:35:16 -0500

    recommit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const testMessage = 'The JavaScript file is connected and running.'
+function testConnection(){
+    console.log('STATUS: The JavaScript file is connected and running.');
+}
+
+let topics = ['dog', 'cat' , 'orca', 'software']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+testConnection();
